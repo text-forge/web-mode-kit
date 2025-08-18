42f98c0d05faff146489be3c021ce8d5f771ade3
mkh-user <khalili1388mahan@gmail.com>
2025-08-18 14:52:19 +0330

    Update JS highlighter colors

--- modes/javascript/mode.gd
+++ modes/javascript/mode.gd
@@ -1,23 +1,24 @@
 extends TextForgeMode
 
 var keyword_colors: Dictionary[Color, Array] = {
-	Color.hex(0xffb366ff): [ "var", "let", "const" ],
-	Color.hex(0xcc99ffff): [ "if", "else", "switch", "case", "default" ],
-	Color.hex(0xff9966ff): [ "for", "while", "do", "break", "continue" ],
-	Color.hex(0x66e0ffff): [ "function", "return", "class", "extends", "constructor", "super" ],
-	Color.hex(0x99ffccff): [ "true", "false", "null", "undefined" ],
-	Color.hex(0xcc66ffff): [ "typeof", "instanceof", "in", "new", "delete", "void" ],
-	Color.hex(0x66ccffff): [ "import", "export", "from", "as" ],
-	Color.hex(0xff6699ff): [ "try", "catch", "finally", "throw" ],
-	Color.hex(0xb3e6ffff): [ "this", "await", "async", "yield", "with", "debugger" ]
+	Color.hex(0xd88c8cff): [ "var", "let", "const" ],
+	Color.hex(0xb38cc4ff): [ "if", "else", "switch", "case", "default" ],
+	Color.hex(0xc48c6bff): [ "for", "while", "do", "break", "continue" ],
+	Color.hex(0x8cc4c4ff): [ "function", "return", "class", "extends", "constructor", "super" ],
+	Color.hex(0x99b38cff): [ "true", "false", "null", "undefined" ],
+	Color.hex(0xa38cc4ff): [ "typeof", "instanceof", "in", "new", "delete", "void" ],
+	Color.hex(0x8cb3c4ff): [ "import", "export", "from", "as" ],
+	Color.hex(0xc48ca3ff): [ "try", "catch", "finally", "throw" ],
+	Color.hex(0xa3c4c4ff): [ "this", "await", "async", "yield", "with", "debugger" ]
 }
+
 var code_regions: Array[Array] = [
-	[Color.hex(0xfff0ccff), '"', '"', false],
-	[Color.hex(0xfff0ccff), "'", "'", false],
-	[Color.hex(0xfff0ccff), "`", "`", false],
+	[Color.hex(0xf2e6ccff), '"', '"', false],
+	[Color.hex(0xf2e6ccff), "'", "'", false],
+	[Color.hex(0xf2e6ccff), "`", "`", false],
 	[Color.hex(0x999999ff), "//", "", true],
 	[Color.hex(0x999999ff), "/*", "*/", false],
-	[Color.hex(0xccccccff), "/**", "*/", false]
+	[Color.hex(0xbfbfbfff), "/**", "*/", false]
 ]
 
 func _initialize_mode() -> Error:
@@ -74,10 +75,10 @@ func _lint_file(text: String) -> Array[Dictionary]:
 
 func _initialize_highlighter() -> void:
 	syntax_highlighter = CodeHighlighter.new()
-	syntax_highlighter.number_color = Color.hex(0xff6666ff)
-	syntax_highlighter.symbol_color = Color.hex(0xffe680ff)
-	syntax_highlighter.function_color = Color.hex(0x66ccffff)
-	syntax_highlighter.member_variable_color = Color.hex(0xccffccff)
+	syntax_highlighter.number_color = Color.hex(0xcc6666ff)
+	syntax_highlighter.symbol_color = Color.hex(0xcccc99ff)
+	syntax_highlighter.function_color = Color.hex(0x99ccccff)
+	syntax_highlighter.member_variable_color = Color.hex(0xb3d9b3ff)
 	for color in keyword_colors:
 		for keyword in keyword_colors[color]:
 			syntax_highlighter.add_keyword_color(keyword, color)
