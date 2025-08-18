afe451b4d31a8f2171d8836f28101576f133aa67
mkh-user <khalili1388mahan@gmail.com>
2025-08-18 15:24:36 +0330

    Add markdown mode

---
+++ modes/markdown/mode.gd
@@ -0,0 +1,36 @@
+extends TextForgeMode
+
+var code_regions: Array[Array] = [
+    [Color.hex(0xb6f1bbff), "`", "`", true],
+    [Color.hex(0xb6f1bbff), "```", "```", false],
+]
+
+func _initialize_mode() -> Error:
+    _initialize_highlighter()
+    comment_delimiters.append({
+        "start_key": "<!--",
+        "end_key": "-->",
+        "line_only": false,
+    })
+    
+    return OK
+
+
+# TODO
+func _generate_preview(text: String) -> String:
+    return String()
+
+
+# TODO
+func _generate_outline(text: String) -> Array:
+    return Array()
+
+
+func _initialize_highlighter() -> void:
+    syntax_highlighter = CodeHighlighter.new()
+    syntax_highlighter.number_color = Color.hex(0xb0e0f6ff)
+    syntax_highlighter.symbol_color = Color.hex(0xd8b6f6ff)
+    syntax_highlighter.function_color = Color.hex(0xffffffff)
+    syntax_highlighter.member_variable_color = Color.hex(0xffffffff)
+    for region in code_regions:
+        syntax_highlighter.add_color_region(region[1], region[2], region[0], region[3])
